b523e002a59c3e429ceac048a42815456ac6b9f2
Dawn <DawnRetrospects@gmail.com>
2020-11-27 15:21:20 -0500

    Potential fix.

--- code/configuration.py
+++ code/configuration.py
@@ -9,7 +9,7 @@ REPO_NAME = "Cathexis"
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = parent_dir(APP_DIR)
 
-FREEZER_DESTINATION = PROJECT_ROOT
+FREEZER_DESTINATION = PROJECT_ROOT # os.path.join(PROJECT_ROOT, 'code/pages')
 FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
 FREEZER_REMOVE_EXTRA_FILES = False
 
